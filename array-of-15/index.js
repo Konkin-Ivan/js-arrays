a0a4a83057a3261a23cc8cf8b703daa7687630a4
Julien Bisconti <julienbi@unity3d.com>
2022-02-02 20:55:55 +0300

    1. Делаю цикл, который генерирует исходный массив в диапазоне [-10, 30].

---
+++ array-of-15/index.js
@@ -0,0 +1,12 @@
+//1. Делаю цикл, который генерирует исходный массив в диапазоне [-10, 30].
+let firstNumber = -10
+let lastNumber = 30
+let arr = []
+for (let i = firstNumber; i < lastNumber + 1; i++) {
+    
+    arr.push(i)
+    
+}
+console.log(arr)
+
+//2. Формирую массив из 15 целых чисел.
