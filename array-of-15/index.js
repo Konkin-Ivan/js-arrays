c69e11a156f6c41479f95e6aacee36f0ba509d55
Julien Bisconti <julienbi@unity3d.com>
2022-02-02 23:26:08 +0300

    final

--- array-of-15/index.js
+++ array-of-15/index.js
@@ -1,12 +1,25 @@
 //1. Делаю цикл, который генерирует исходный массив в диапазоне [-10, 30].
-let firstNumber = -10
-let lastNumber = 30
+const minNumber = -10
+const maxNumber = 30
 let arr = []
-for (let i = firstNumber; i < lastNumber + 1; i++) {
+for (let i = minNumber; i < maxNumber + 1; i++) {
     
     arr.push(i)
     
 }
-console.log(arr)
 
-//2. Формирую массив из 15 целых чисел.
+//2. Формирую массив 15 целых чисел из диапазона [-10, 30].
+let randomArr = arr.sort(() => Math.random() - 0.5)
+const setRange = 15
+let filterResult = randomArr.slice(0, setRange)
+
+
+//3. Нахожу среднее арифмитическое.
+let total = 0
+const li = filterResult.length
+for (let n in filterResult) {
+    total += filterResult[n]
+}
+const averageNumber = total / li
+
+console.log(averageNumber)
